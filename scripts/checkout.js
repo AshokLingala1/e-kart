81505bdedb078986d41a5c0863c48ef55ffa2f74
AshokLingala01 <ashoklingala26@gmail.com>
2024-07-02 12:27:54 +0530

    Committing all the files

---
+++ scripts/checkout.js
@@ -0,0 +1,7 @@
+import {renderOrderSummary,renderCartQuantity} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+
+renderCartQuantity();
+renderOrderSummary();
+renderPaymentSummary();
+
